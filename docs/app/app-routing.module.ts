e65f34949a8d2b0c291797ef355db79645d34bba
maaicondgl <maaicondgl@outlook.com>
2021-11-22 19:25:39 -0300

    nova branch

---
+++ docs/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { ProdutosComponent } from './views/produtos/produtos.component';
+import { SlideComponent } from './views/slide/slide.component';
+
+const routes: Routes = [
+  {path:"", component:HomeComponent},
+  {path:"produtos", component:ProdutosComponent},
+  {path:"login", component:LoginComponent},
+  {path:"slide", component:SlideComponent},
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
